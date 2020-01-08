b08edbb511a5c7c252702e71e5368b476ffb0d85
ishe19 <38261770+ishe19@users.noreply.github.com>
2020-01-08 20:54:58 +0800

    dynamically changing canvas model posts and lists

--- lib/ui/pages/view_post.dart
+++ lib/ui/pages/view_post.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:copy_cat/ui/utils/uidata.dart';
+
 
 class ViewPost extends StatefulWidget {
   String postName;
@@ -9,6 +11,43 @@ class ViewPost extends StatefulWidget {
 }
 
 class _ViewPostState extends State<ViewPost> {
+
+  TextEditingController controller;
+  TextEditingController _editController = new TextEditingController();
+
+  String edit;
+  List items = [
+    "Hello",
+    "World"
+  ];
+
+
+  
+  @override
+  void initState(){
+    super.initState();
+    _editController.addListener((){
+      setState(() {
+        edit = _editController.text;
+      });
+    });
+  }
+
+  void upDateList(int index, String editedPost){
+    setState(() {
+      items.removeAt(index);
+      items.replaceRange(index, index, ["$editedPost, $index"]);
+      print(items);
+    });
+  }
+
+  void newPost(String post){
+
+    setState(() {
+      items.add(post);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,22 +55,91 @@ class _ViewPostState extends State<ViewPost> {
         title: Text(widget.postName),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: ,
+        onPressed: (){
+          newPost("post");
+        },
+        backgroundColor: Uidata.btnColor,
         child: Icon(Icons.add),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(Icons.pages),
-                Text("Add post")
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: Container(
+          child: ListView.builder(
+                      itemCount: items.length,
+                      itemBuilder: (BuildContext context, int index){
+                        return viewCard(items[index], index);
+                      }),
+        )
+    );
+  }
+
+  Widget viewCard(String post, int index){
+
+    FocusNode _focusNode = new FocusNode();
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child:Card(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+
+
+                    Row(
+                      children: <Widget>[
+                        Text(post, style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          width: 240,
+                          child: EditableText(
+                            controller: _editController,
+                            focusNode: _focusNode,
+                            style: TextStyle(color:  Colors.black),
+                            cursorColor: Colors.black,
+                            backgroundCursorColor: Colors.white,
+                            forceLine: true,
+//                            onSubmitted: ,
+
+                          )
+                        )
+                      ],
+
+                    ),
+                    Row(
+                      children: <Widget>[
+                        FlatButton(
+                          //                splashColor: Colors.grey,
+                          color: Colors.white,
+                          child: Text("EDIT POST", style: TextStyle(color: Uidata.primaryColor),),
+                          onPressed: (){
+//                            Navigator.push(context, MaterialPageRoute(builder: (context) => EditPost()));
+                          upDateList(index, edit);
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+        )
+    );
+  }
+
+}
+
+class EditPost extends StatefulWidget {
+  @override
+  _EditPostState createState() => _EditPostState();
+}
+
+class _EditPostState extends State<EditPost> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
     );
   }
 }
